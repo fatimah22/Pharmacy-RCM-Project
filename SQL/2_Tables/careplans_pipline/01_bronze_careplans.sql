c88f32b4325191901e7a54992e1987455496791e
Fatimah <fatmah.od@gmail.com>
2026-07-01 12:43:01 +0300

    Create 01_bronze_careplans.sql

---
+++ SQL/2_Tables/careplans_pipline/01_bronze_careplans.sql
@@ -0,0 +1,33 @@
+-- =========================================================
+-- CAREPLANS TABLE
+-- 01_bronze_careplans.sql
+-- Purpose: Create and load the raw careplans table in Bronze
+-- =========================================================
+
+IF OBJECT_ID('bronze.careplans', 'U') IS NOT NULL
+    DROP TABLE bronze.careplans;
+
+CREATE TABLE bronze.careplans (
+    ID NVARCHAR(100),
+    Start_Date DATE,
+    Stop_Date DATE,
+    Patient_Code NVARCHAR(100),
+    Encounter_Code NVARCHAR(100),
+    Code NVARCHAR(50),
+    Description NVARCHAR(300),
+    Reason_Code NVARCHAR(50),
+    Reason_Description NVARCHAR(300)
+);
+
+TRUNCATE TABLE bronze.careplans;
+
+BULK INSERT bronze.careplans
+FROM 'C:\Users\fatima abdullah\OneDrive\Desktop\Projects\Hospital\100k_synthea_covid19_csv\careplans.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0A',
+    CODEPAGE = '65001',
+    TABLOCK
+);
