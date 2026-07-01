8e3d1e8bbe466742f6486ddba3cf8bdcc4973be2
Fatimah <fatmah.od@gmail.com>
2026-07-01 13:39:08 +0300

    Create 04_gold_conditions.sql

---
+++ SQL/2_Tables/conditions_pipeline/04_gold_conditions.sql
@@ -0,0 +1,16 @@
+-- =========================================================
+-- CONDITIONS TABLE
+-- 04_gold_conditions.sql
+-- Purpose: Create the Gold-layer reporting view for conditions
+-- =========================================================
+
+CREATE OR ALTER VIEW gold.conditions AS
+SELECT
+    Start_Date,
+    Stop_Date,
+    Patient_Code,
+    Encounter_Code,
+    Reason_Code,
+    Reason_Description,
+    dq_reason_code_conflict_flag
+FROM silver.conditions;
