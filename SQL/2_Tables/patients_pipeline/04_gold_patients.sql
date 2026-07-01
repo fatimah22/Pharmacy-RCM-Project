093c86bebf945d296d363ec05ad014865c94ca7e
Fatimah <fatmah.od@gmail.com>
2026-07-01 14:09:19 +0300

    Create 04_gold_patients.sql

---
+++ SQL/2_Tables/patients_pipeline/04_gold_patients.sql
@@ -0,0 +1,35 @@
+-- =========================================================
+-- PATIENTS TABLE
+-- 04_gold_patients.sql
+-- Purpose: Create the Gold-layer reporting view for patients
+-- =========================================================
+
+CREATE OR ALTER VIEW gold.patients AS
+SELECT
+    Patient_Code,
+    Birthdate,
+    Deathdate,
+    CASE
+        WHEN Deathdate IS NULL THEN DATEDIFF(YEAR, Birthdate, GETDATE())
+        WHEN Deathdate IS NOT NULL THEN DATEDIFF(YEAR, Birthdate, Deathdate)
+    END AS Age,
+    First_Name,
+    Last_Name,
+    Maiden_Name,
+    Marital_Status,
+    Race,
+    Ethnicity,
+    Gender,
+    Birth_City,
+    Birth_State_Province,
+    Birth_Country_Code,
+    Address,
+    City,
+    State,
+    Country,
+    ZIP,
+    LAT,
+    LON,
+    Health_Care_Expenses,
+    Health_Care_Coverage
+FROM silver.patients;
