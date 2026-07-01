d2ee59dc14687c9f2eef4fd04435ab92eeeafd3e
Fatimah <fatmah.od@gmail.com>
2026-07-01 13:17:42 +0300

    Update 04_gold_careplans.sql

--- SQL/2_Tables/careplans_pipline/04_gold_careplans.sql
+++ SQL/2_Tables/careplans_pipline/04_gold_careplans.sql
@@ -4,16 +4,31 @@
 -- Purpose: Create the Gold-layer reporting view for careplans
 -- =========================================================
 
-CREATE OR ALTER VIEW gold.careplans AS
+
+CREATE OR ALTER VIEW gold.claims_main AS
 SELECT
-    Careplans_Code,
-    Start_Date,
-    Stop_Date,
-    Patient_Code,
-    Encounter_Code,
-    Code,
-    Description,
-    Reason_Code,
-    Reason_Description,
-    dq_reason_code_conflict_flag
-FROM silver.careplans;
+    Claim_ID,
+    Claim_Submission_Date,
+    Claim_Year,
+    Claim_Quarter,
+    Payer_Type,
+    Provider_Specialty,
+    Place_of_Service_Code,
+    place_of_service_descreption,
+    CPT_Code,
+    Modifier,
+    Primary_ICD10_dx,
+    Primary_ICD10_desc,
+    Secondary_ICD10_dx,
+    Secondary_DX_Count,
+    Prior_Auth_Required,
+    Prior_Auth_Obtained,
+    Prior_Auth_Number,
+    Documentation_Completeness,
+    Claim_Amount_USD,
+    Outcome,
+    Denial_Reason_Code,
+    Denial_Category,
+    Synthetic_Flag,
+    Generation_Date
+FROM silver.claims_main;
