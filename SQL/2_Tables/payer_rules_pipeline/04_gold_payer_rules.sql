0535615d111fe6155b21ef524567db21cc595d7e
Fatimah <fatmah.od@gmail.com>
2026-07-03 22:30:01 +0300

    Create 04_gold_payer_rules.sql

---
+++ SQL/2_Tables/payer_rules_pipeline/04_gold_payer_rules.sql
@@ -0,0 +1,17 @@
+-- =========================================================
+-- PAYER RULES TABLE
+-- 04_gold_payer_rules.sql
+-- Purpose: Create the Gold-layer reporting view for payer_rules
+-- =========================================================
+
+CREATE OR ALTER VIEW gold.payer_rules AS
+SELECT
+    Payer_Type,
+    CPT_Code,
+    Requires_Prior_Auth,
+    Auth_Lead_Time_Days,
+    Historical_Denial_Rate,
+    Avg_Payment_Turnaround_Days,
+    Timely_Filing_Limit_Days,
+    Dataset_Version
+FROM silver.payer_rules;
