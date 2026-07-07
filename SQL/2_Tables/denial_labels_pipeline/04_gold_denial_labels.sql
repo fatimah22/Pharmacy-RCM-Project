72dcd7a81cdd35cae1bf26935c84c39332b11594
Fatimah <fatmah.od@gmail.com>
2026-07-07 20:40:54 +0300

    Update 04_gold_denial_labels.sql

--- SQL/2_Tables/denial_labels_pipeline/04_gold_denial_labels.sql
+++ SQL/2_Tables/denial_labels_pipeline/04_gold_denial_labels.sql
@@ -4,14 +4,22 @@
 -- Purpose: Create the Gold-layer reporting view for denial_labels
 -- =========================================================
 
-CREATE OR ALTER VIEW gold.denial_labels AS
+CREATE OR ALTER VIEW gold.fact_denial_labels AS
 SELECT
     Claim_ID,
     Denial_Category,
     Denial_Reason_Code,
-    Denial_Code_Description,
     Appealable,
     Appeal_Success_Probability,
     Recovery_Action,
     Estimated_Recovery_USD
 FROM silver.denial_labels;
+
+
+-- create dimension tabel 
+CREATE OR ALTER VIEW gold.dim_denial_reason AS
+SELECT DISTINCT
+    Denial_Reason_Code,
+    Denial_Code_Description
+FROM silver.denial_labels;
+
