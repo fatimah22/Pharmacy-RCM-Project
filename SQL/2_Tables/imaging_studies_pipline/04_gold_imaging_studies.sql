1539f0caa268e23412ab302c2c503d66eb1f3fc3
Fatimah <fatmah.od@gmail.com>
2026-07-07 20:53:29 +0300

    Update 04_gold_imaging_studies.sql

--- SQL/2_Tables/imaging_studies_pipline/04_gold_imaging_studies.sql
+++ SQL/2_Tables/imaging_studies_pipline/04_gold_imaging_studies.sql
@@ -4,16 +4,37 @@
 -- Purpose: Create the Gold-layer reporting view for imaging_studies
 -- =========================================================
 
-CREATE OR ALTER VIEW gold.imaging_studies AS
+
+CREATE OR ALTER VIEW gold.fac_imaging_studies AS
 SELECT
-    ID,
+    Imaging_ID,
     [Date],
     Patient_Code,
     Encounter_Code,
     Bodysite_Code,
-    Bodysite_Descreption,
     Modality_Code,
-    Modality_Description,
+    SOP_Code
+FROM silver.imaging_studies;
+
+
+------- 
+--Create the dimension tables out of imaging studies fact tabel 
+CREATE OR ALTER VIEW gold.dim_Bodysite AS
+SELECT DISTINCT
+    Bodysite_Code,
+    Bodysite_Descreption
+FROM silver.imaging_studies;
+
+---------
+CREATE OR ALTER VIEW gold.dim_Modality AS
+SELECT DISTINCT
+    Modality_Code,
+    Modality_Description
+FROM silver.imaging_studies
+
+---------
+CREATE OR ALTER VIEW gold.dim_SOP AS
+SELECT DISTINCT
     SOP_Code,
     SOP_Description
-FROM silver.imaging_studies;
+FROM silver.imaging_studies
