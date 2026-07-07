65b351c057ca880a25e96d6153f97311c396f68d
Fatimah <fatmah.od@gmail.com>
2026-07-07 20:45:53 +0300

    Update 04_gold_devices.sql

--- SQL/2_Tables/devices_pipeline/04_gold_devices.sql
+++ SQL/2_Tables/devices_pipeline/04_gold_devices.sql
@@ -4,13 +4,20 @@
 -- Purpose: Create the Gold-layer reporting view for devices
 -- =========================================================
 
-CREATE OR ALTER VIEW gold.devices AS
+CREATE OR ALTER VIEW gold.fact_devices AS
 SELECT
     Start_Date,
     Stop_Date,
     Patient_Code,
     Encounter_Code,
-    Code,
-    Description,
+    Device_Code,
     Unique_Device_Identification_UDI
+FROM silver.devices;
+
+
+-- Create dimension table 
+CREATE OR ALTER VIEW gold.dim_device_type AS
+SELECT DISTINCT
+    Device_Code,
+    Device_Description
 FROM silver.devices;
