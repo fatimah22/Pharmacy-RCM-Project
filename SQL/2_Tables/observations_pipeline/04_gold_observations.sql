3ae814419184defed56924a53ce73869f265789e
Fatimah <fatmah.od@gmail.com>
2026-07-08 19:42:15 +0300

    Update 04_gold_observations.sql

--- SQL/2_Tables/observations_pipeline/04_gold_observations.sql
+++ SQL/2_Tables/observations_pipeline/04_gold_observations.sql
@@ -4,8 +4,11 @@
 -- Purpose: Create the Gold-layer reporting view for observations
 -- =========================================================
 
-CREATE OR ALTER VIEW gold.observations AS
+-- CREATE fact_encounter_outcomes 
+CREATE OR ALTER VIEW gold.fact_observations_encounter AS
+
 SELECT
+    'Obs_enc_' + RIGHT('000' + CAST(ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS VARCHAR(10)), 3) AS Obs_enc_ID,
     Date,
     Patient_Code,
     Encounter_Code,
@@ -13,8 +16,24 @@ SELECT
     Description,
     Value,
     Unit,
-    Type,
-    Code_Description_Flag,
-    Patient_level_outcome_observations_flag,
-    Encounter_based_observations_flag
-FROM silver.observations;
+    Code_Description_Flag
+FROM silver.observations
+WHERE Encounter_based_observations_flag =1 
+
+--------------------------------------------------
+-- CREATE fact_patient_outcomes 
+
+
+CREATE OR ALTER VIEW gold.fact_observations_patient_outcomes AS
+
+SELECT
+'Obs_pat_' + RIGHT('000' + CAST(ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS VARCHAR(10)), 3) AS Obs_pat_ID,
+    Date,
+    Patient_Code,
+    Code,
+    Description,
+    Value,
+    Unit,
+    Code_Description_Flag
+FROM silver.observations
+WHERE Patient_level_outcome_observations_flag =1 ;
