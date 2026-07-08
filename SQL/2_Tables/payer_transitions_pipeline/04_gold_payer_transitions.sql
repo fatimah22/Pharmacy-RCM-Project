a5aece01073a62c67ecea63ef33a162590d24540
Fatimah <fatmah.od@gmail.com>
2026-07-08 20:41:19 +0300

    Update 04_gold_payer_transitions.sql

--- SQL/2_Tables/payer_transitions_pipeline/04_gold_payer_transitions.sql
+++ SQL/2_Tables/payer_transitions_pipeline/04_gold_payer_transitions.sql
@@ -4,11 +4,14 @@
 -- Purpose: Create the Gold-layer reporting view for payer_transitions
 -- =========================================================
 
-CREATE OR ALTER VIEW gold.payer_transitions AS
+
+CREATE OR ALTER VIEW gold.fact_payer_transitions AS
 SELECT
     Patient_Code,
     Start_Year,
+    DATEFROMPARTS( start_year , 01,01) AS Start_Date,
     End_Year,
+    DATEFROMPARTS( End_year , 01,01) AS End_Date,
     Payer_Code,
     Ownership
 FROM silver.payer_transitions;
