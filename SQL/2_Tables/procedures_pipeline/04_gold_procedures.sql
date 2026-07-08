207433c48c36d2a3dbf340f73cc596d9b49c7ea8
Fatimah <fatmah.od@gmail.com>
2026-07-08 20:48:15 +0300

    Update 04_gold_procedures.sql

--- SQL/2_Tables/procedures_pipeline/04_gold_procedures.sql
+++ SQL/2_Tables/procedures_pipeline/04_gold_procedures.sql
@@ -4,15 +4,21 @@
 -- Purpose: Create the Gold-layer reporting view for procedures
 -- =========================================================
 
-CREATE OR ALTER VIEW gold.procedures AS
+CREATE OR ALTER VIEW gold.fact_procedures AS
 SELECT
     Date,
     Patient_Code,
     Encounter_Code,
-    Code,
-    Description,
+    Procedure_Code,
     Base_Cost,
     Reason_Code,
     Reason_Description,
     Code_Description_Flag
+FROM silver.procedures;
+
+-- create dimention table
+CREATE OR ALTER VIEW gold.dim_procedure_disc AS
+SELECT DISTINCT
+    Procedure_Code,
+    Procedure_Description
 FROM silver.procedures;
