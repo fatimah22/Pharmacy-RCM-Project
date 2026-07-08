b4754e5a2296c238a8665d731b7ddb09d3967c70
Fatimah <fatmah.od@gmail.com>
2026-07-08 19:46:22 +0300

    Update 03_silver_payer_rules.sql

--- SQL/2_Tables/payer_rules_pipeline/03_silver_payer_rules.sql
+++ SQL/2_Tables/payer_rules_pipeline/03_silver_payer_rules.sql
@@ -1,18 +1,13 @@
--- =========================================================
--- PAYER RULES TABLE
--- 03_silver_payer_rules.sql
--- Purpose: Create and load the cleaned payer_rules table in Silver
--- =========================================================
-
 IF OBJECT_ID('silver.payer_rules', 'U') IS NOT NULL
     DROP TABLE silver.payer_rules;
 
 CREATE TABLE silver.payer_rules (
+    Payer_Rule_Code NVARCHAR(50),
     Payer_Type NVARCHAR(50),
     CPT_Code NVARCHAR(50),
     Requires_Prior_Auth INT,
     Auth_Lead_Time_Days INT,
-    Historical_Denial_Rate DECIMAL(10,2),
+    Historical_Denial_Rate DECIMAL (10,2),
     Avg_Payment_Turnaround_Days INT,
     Timely_Filing_Limit_Days INT,
     Dataset_Version INT
@@ -21,26 +16,24 @@ CREATE TABLE silver.payer_rules (
 TRUNCATE TABLE silver.payer_rules;
 
 INSERT INTO silver.payer_rules (
-    Payer_Type,
-    CPT_Code,
-    Requires_Prior_Auth,
-    Auth_Lead_Time_Days,
-    Historical_Denial_Rate,
-    Avg_Payment_Turnaround_Days,
-    Timely_Filing_Limit_Days,
-    Dataset_Version
+    Payer_Rule_Code,
+    Payer_Type ,
+    CPT_Code ,
+    Requires_Prior_Auth ,
+    Auth_Lead_Time_Days ,
+    Historical_Denial_Rate ,
+    Avg_Payment_Turnaround_Days ,
+    Timely_Filing_Limit_Days ,
+    Dataset_Version 
 )
 SELECT
-    NULLIF(TRIM(Payer_Type), '') AS Payer_Type,
+    'P-' + RIGHT('000' + CAST(ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS VARCHAR(10)), 3) AS Payer_Rule_Code,
+    NULLIF(TRIM( Payer_Type), '') AS  Payer_Type,
     NULLIF(TRIM(CPT_Code), '') AS CPT_Code,
-    Requires_Prior_Auth,
-    Auth_Lead_Time_Days,
-    CAST(Historical_Denial_Rate AS DECIMAL(10,2)) AS Historical_Denial_Rate,
-    Avg_Payment_Turnaround_Days,
-    Timely_Filing_Limit_Days,
-    Dataset_Version
+    Requires_Prior_Auth ,
+    Auth_Lead_Time_Days ,
+    Historical_Denial_Rate ,
+    Avg_Payment_Turnaround_Days ,
+    Timely_Filing_Limit_Days ,
+    Dataset_Version 
 FROM bronze.payer_rules;
-
--- Post-load validation
-SELECT *
-FROM silver.payer_rules;
