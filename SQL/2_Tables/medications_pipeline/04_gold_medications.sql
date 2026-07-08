32d426a7012b27282ff21e27b2f677ecacbcaae4
Fatimah <fatmah.od@gmail.com>
2026-07-08 19:40:16 +0300

    Update 04_gold_medications.sql

--- SQL/2_Tables/medications_pipeline/04_gold_medications.sql
+++ SQL/2_Tables/medications_pipeline/04_gold_medications.sql
@@ -4,20 +4,27 @@
 -- Purpose: Create the Gold-layer reporting view for medications
 -- =========================================================
 
-CREATE OR ALTER VIEW gold.medications AS
+CREATE OR ALTER VIEW gold.fact_medications AS
 SELECT
+    Medication_ID,
     Start_Date,
     Stop_Date,
     Patient_Code,
     Payer_Code,
     Encounter_Code,
     Code,
-    Description,
     Base_Cost,
     Payer_Coverage,
     Dispenses,
     Total_Cost,
     Reason_Code,
-    Reason_Description,
-    Code_Description_Flag
+    Reason_Description
+FROM silver.medications;
+
+-- create dimension table 
+CREATE OR ALTER VIEW gold.dim_medication AS 
+SELECT DISTINCT
+Code,
+Description,
+Code_Description_Flag
 FROM silver.medications;
