ffd80e338d5204ff3708905f225a5c1a74ea7f05
Fatimah <fatmah.od@gmail.com>
2026-07-08 19:36:01 +0300

    Update 04_gold_immunizations.sql

--- SQL/2_Tables/immunizations_pipeline/04_gold_immunizations.sql
+++ SQL/2_Tables/immunizations_pipeline/04_gold_immunizations.sql
@@ -4,12 +4,19 @@
 -- Purpose: Create the Gold-layer reporting view for immunizations
 -- =========================================================
 
-CREATE OR ALTER VIEW gold.immunizations AS
+
+CREATE OR ALTER VIEW gold.fact_immunizations AS
 SELECT
     [Date],
     Patient_Code,
     Encounter_Code,
     Immunizations_Code,
-    Immunizations_Code_Description,
-    Best_Cost
+    base_Cost
+FROM silver.immunizations;
+
+--create dimension tabel 
+CREATE OR ALTER VIEW gold.dim_immunizations AS
+SELECT DISTINCT 
+    Immunizations_Code,
+    Immunizations_Code_Description
 FROM silver.immunizations;
